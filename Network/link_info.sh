3f694e1420cbdcdc64fc9e2f0677870bb13945c7
DearTanker <1122669+DearTanker@users.noreply.github.com>
2018-10-18 14:45:03 +0800

    Update Network/link_info.sh
    Add link_info.sh

---
+++ Network/link_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tmpfile=$(mktemp)
+netstat -anp | grep 'ESTABLISHED' | grep 'python' | grep 'tcp' | awk '{print $4,$5}' | grep -v '0.0.0.0'| awk -F '[:| ]' '$2>8000 && $2<9999{print $0}'  > ${tmpfile}
+for port in `cat ${tmpfile} | awk -F '[:| ]' '{print $2}' | sort| uniq`
+do
+  num=$(grep ":${port} " ${tmpfile} | awk -F '[:| ]' '{print $3}' | sort |uniq| wc -l)
+  echo "${port} ----> [${num}] $(grep ":${port} " ${tmpfile} | awk -F '[:| ]' '{print $3}' | sort |uniq |xargs | sed 's/ /,/g')"
+done
