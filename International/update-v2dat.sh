693a193a21b32b4d4f768df7b05a2edaac369fce
DearTanker <1122669+DearTanker@users.noreply.github.com>
2020-05-22 02:04:15 +0800

    Add files via upload

---
+++ International/update-v2dat.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+v2ray_folder="/usr/bin/v2ray/"
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
+
+echo -e "${GREEN}>>> change directory...${NC}"
+cd $v2ray_folder
+
+echo -e "${GREEN}>>> delete old dat files...${NC}"
+rm -f geoip.dat geosite.dat
+
+echo -e "${GREEN}>>> downloading geoip.dat files...${NC}"
+curl -L -O $GEOIP_URL
+
+echo -e "${GREEN}>>> downloading geosite.dat files...${NC}"
+curl -L -O $GEOSITE_URL
+
+echo -e "${GREEN}Done!${NC}"
